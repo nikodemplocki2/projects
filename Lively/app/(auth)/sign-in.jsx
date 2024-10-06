377b1431f39f10bfb8318e6ce77a58480ce4bb30
nikodemplocki2 <plocki.nik@gmail.com>
2024-10-06 23:37:29 +0200

    Initial Commit

---
+++ Lively/app/(auth)/sign-in.jsx
@@ -0,0 +1,43 @@
+import { View, Text, ScrollView, Image } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView} from 'react-native-safe-area-context'
+
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
+
+
+
+const SignIn = () => {
+  const [Form, setForm] = useState({
+    email:'',
+    password:''
+  })
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image 
+          source={images.logo}
+          className="w-[115px] h-[35px]"
+          resizeMode="contain"
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
+          <FormField 
+            title="Email"
+            value={Form.email}
+            handleChangeText={(e) => setForm({ ...Form, email: e})} otherStyles="mt-7" keyboardType="email-address"
+          />
+
+          <FormField 
+            title="Password"
+            value={Form.password}
+            handleChangeText={(e) => setForm({ ...Form, password: e})} otherStyles="mt-7"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default SignIn
